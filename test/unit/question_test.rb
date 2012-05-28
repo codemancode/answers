7e4f64991b065d8e50c39d0686cb60808a0eb69b
Matthew McFarling <matthew.mcfarling@gmail.com>
2012-05-28 11:21:39 -0400

    adding question and answer unit tests

--- test/unit/question_test.rb
+++ test/unit/question_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @q = questions(:question_one)
+  end
+  
+  test "invalid without a title" do
+    @q.title = nil
+    assert @q.valid?, "Title is not being validated."
+  end
+
+  test "invalid without question details" do
+    @q.details = nil
+    assert @q.valid?, "Details is not being validated."
+  end
+
+  test "valid with all attributes" do
+    @q = questions(:question_one)
+    assert @q.valid?, "Question is not valid."
+  end
+
+  test "should contain answers to a question" do
+    assert @q.answers.all? { |answer| answer.question == @q }
+  end
 end
