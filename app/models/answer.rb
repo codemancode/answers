40810bda0ac401956c2d4c0cb8f037cfd99437f0
Matthew McFarling <matthew.mcfarling@gmail.com>
2012-05-28 11:20:53 -0400

    adding answer and question models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,6 @@
 class Answer < ActiveRecord::Base
   attr_accessible :answer, :question_id
+
+  # Associations
+  belongs_to :question
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,10 @@
 class Question < ActiveRecord::Base
   attr_accessible :details, :title
+
+  # Associations
+  has_many :answers
+
+  # Validations
+  validates_presence_of :title, :on => :create, :message => "can't be blank"
+  validates_presence_of :details, :on => :create, :message => "can't be blank"
 end
